cb15f4bc959ad6e9b05ed90612eae0f930755297
Teodora <teodora.cerevicki@gmail.com>
2023-10-18 11:59:37 +0200

    Removed unused files, added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import './App.css';
-
-import {useState, useEffect} from 'react';
-import Post from './Post';
+import { useState, useEffect } from "react";
+import Post from "./Post";
 import axios from "axios";
-
 
 function App() {
   const [posts, setPosts] = useState([]);
-  
+
   const client = axios.create({
-    baseURL: 'https://api.spacexdata.com/v3/launches'
-  })
-  
-  const fetchPosts = async() => {
+    baseURL: "https://api.spacexdata.com/v3/launches",
+  });
+
+  const fetchPosts = async () => {
     const response = await client.get();
     setPosts(response.data);
-  }
- 
- useEffect(() => {
-      fetchPosts()
-   }, []);
-   
-   
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  });
+
   return (
-		<main>
-			<div className="container">
-					<div className='row'>
-						<div className='col-12 col-md-4 col-lg-3'>
-								{posts.map((post, index) => 
-									<Post 
-										key={index} 
-										mission_name={post.mission_name}
-										launch_date_utc={post.launch_date_utc} 
-										details={post.details}
-									/>
-								)}
-						</div>
-					</div>
-			</div>
-	</main>
-  )
+    <main>
+      <div className="container">
+        <div className="row">
+          {posts.map((post, index) => (
+            <Post
+              key={index}
+              mission_name={post.mission_name}
+              launch_date_utc={post.launch_date_utc}
+              details={post.details}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,19 @@
-import rocket from './images/rocket_launch.svg';
+import rocket from "./images/rocket_launch.svg";
 
 export default function Post(props) {
-    return (
-        <section className="card">
-            <header>
-                <img className='image' alt='rocket' src={rocket} />
-                <h1 className="title">id {props.mission_name}</h1>
-            </header>
+  return (
+    <div className="col-12 col-md-4 col-lg-3">
+      <section className="card">
+        <header>
+          <img className="image" alt="rocket" src={rocket} />
+          <h1 className="title">id {props.mission_name}</h1>
+        </header>
 
-            <div className='content'>
-                <p className="">{props.launch_date_utc}</p>
-                <p>{props.details}</p>
-            </div>
-        </section>
-    )   
+        <div className="content">
+          <p className="">{props.launch_date_utc}</p>
+          <p>{props.details}</p>
+        </div>
+      </section>
+    </div>
+  );
 }
