6e18414c987f940245c0cb24c5c38dd4cd110e87
Teodora <teodora.cerevicki@gmail.com>
2023-10-19 00:35:43 +0200

    implemented theme toggler

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
 import rocket from "../../images/rocket_launch.svg";
+import ThemeToggler from "../theme-toggler/DarkMode";
 
 export default function Header() {
   return (
     <header className="main-header">
-        <div className="container">
+        <div className="container wrapper">
             <img className="image" alt="company logo" src={rocket} />
+
+            <ThemeToggler />
         </div>
     </header>
   )
