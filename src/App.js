4e66fffb8a9aec55f0f8ad72a47fa3977cd7d534
Teodora <teodora.cerevicki@gmail.com>
2023-10-18 13:52:24 +0200

    refactoring: moved cards and created launch compnent

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import { useState, useEffect } from "react";
-import Post from "./Post";
-import axios from "axios";
+import Launch from "./components/launchs/Launch";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  const client = axios.create({
-    baseURL: "https://api.spacexdata.com/v3/launches",
-  });
-
-  const fetchPosts = async () => {
-    const response = await client.get();
-    setPosts(response.data);
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  });
-
   return (
     <main>
-      <div className="container">
-        <div className="row">
-          {posts.map((post, index) => (
-            <Post
-              key={index}
-              mission_name={post.mission_name}
-              launch_date_utc={post.launch_date_utc}
-              details={post.details}
-            />
-          ))}
-        </div>
-      </div>
+		<Launch />
     </main>
   );
 }
