7f74714905edaf03668dd2c1d41d148306a6c42e
Teodora <teodora.cerevicki@gmail.com>
2023-10-22 23:12:07 +0200

    code clean up

--- src/components/launchs/Launch.js
+++ src/components/launchs/Launch.js
@@ -2,41 +2,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./Card";
 
-// export default function Launch() {
-//     const [posts, setPosts] = useState([]);
-
-//     const client = axios.create({
-//       baseURL: "https://api.spacexdata.com/v3/launches",
-//     });
-  
-//     const fetchPosts = async () => {
-//       const response = await client.get();
-//       setPosts(response.data);
-//     };
-  
-//     useEffect(() => {
-//       fetchPosts();
-//     });
-//   return (
-//     <div className="container">
-//         <div className="row">
-
-//           <div className="col-12">
-//             <input className="search-field" type="search" placeholder="Search launch"/>
-//           </div>
-//           {posts.map((post, index) => (
-//             <Card
-//               key={index}
-//               mission_name={post.mission_name}
-//               launch_date_utc={post.launch_date_utc}
-//               details={post.details}
-//             />
-//           ))}
-//         </div>
-//     </div>
-//   )
-// }
-
 function Launch() {
   const [data, setData] = useState([]);
   const [records, setRecords] = useState([]);
